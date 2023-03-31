7f1c8f3f68beadad17622b64ed950380dd275474
olgaodc <olgaodincova@gmail.com>
2023-04-01 02:59:43 +0300

    responsive added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const aboutSection = document.getElementById('about');
 
 welcomeButton.addEventListener('click', () => {
     aboutSection.scrollIntoView({ 
-        block: 'start', // к ближайшей границе экрана
-        behavior: 'smooth', // и плавно 
+        block: 'start',
+        behavior: 'smooth',
     });
 })
